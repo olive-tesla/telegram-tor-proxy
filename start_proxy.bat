ad227f8f98dcf10d744b96af54b68e07ffe9f697
olive.tesla <231417267+olive-tesla@users.noreply.github.com>
2026-05-21 23:23:15 +0700

    start_proxy.bat simplified

--- start_proxy.bat
+++ start_proxy.bat
@@ -1,18 +1,3 @@
 @echo off
-
-REM 1. Проверка наличия Python
-python --version >nul 2>&1
-if %errorlevel% neq 0 (
-    echo [!] Python was not found. Please, download it from: python.org
-    echo Check "Add Python to PATH" during setup.
-    pause
-    exit /b
-)
-
-REM 2. Просто запускаем main.py, далее он сам разберётся, что делать
 python main.py
-
 pause
-
-
-
